fc30dc90312df9f66a68c26f745bf9d8ebe7efec
jonathanon141 <janor360@gmail.com>
2023-06-15 15:06:50 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/* this prints "Programming is like building a multili    ngual puzzle */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
