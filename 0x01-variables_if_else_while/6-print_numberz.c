630c05188c83d583227035fcc39392776603565d
jonathanon141 <janor360@gmail.com>
2023-06-16 17:37:55 -0700

    c programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entery poin
+ *Return: always 0
+ */
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
