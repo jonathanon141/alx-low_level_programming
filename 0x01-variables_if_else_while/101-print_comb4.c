fad0b5bc3b9507addf4322110b2b73a74ee66166
jonathanon141 <janor360@gmail.com>
2023-06-16 21:24:04 -0700

    advance

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ *main - entery point
+ *Return: always 0
+ */
+int main(void)
+{
+	int a = '0';
+	int b = '0';
+	int c = '0';
+
+	while (a <= '7')
+	{
+		while (b <= '8')
+		{
+			while (c = '9')
+			{
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (!(a == '7' &&  b == '8' && c == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				c++;
+			}
+			c = '0';
+			b++;
+		}
+		b = '0';
+		a++;
+	}
+	putchar('\n');
+	return (0);
+
+}
